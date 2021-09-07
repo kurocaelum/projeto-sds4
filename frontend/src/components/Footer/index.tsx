2c0a8f1ec084c4128226c827c3bdeb615d2bba68
Victor <victor_brandao@outlook.com>
2021-09-07 04:53:54 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <footer className='footer mt-auto py-3 bg-dark'>
+      <div className='container'>
+        <p className='text-light'>
+          App desenvolvido por{' '}
+          <a href='https://github.com/kurocaelum/' target='_blank' rel='noreferrer'>
+            Victor Simplício
+          </a>
+        </p>
+        <p className='text-light'>
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{' '}
+            <a href='https://instagram.com/devsuperior.ig' target='_blank' rel='noreferrer'>
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
